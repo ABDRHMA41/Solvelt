65e0c48cd429468461125997444e43116de9a91f
abde <Tbdalrhmnaghbsh@gmail>
2026-05-31 03:08:54 -0700

    Add file

--- Solvelt.cpp
+++ Solvelt.cpp
@@ -1,1 +1,28 @@
-#include <iostream>int main(){	std::cout << "Hi \n";	return 0;}
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout << "Enter the value of a,b and c: ";
+	cin >> a >> b >> c;
+	if (a == 0)
+	{
+		cout << "The equation is not quadratic.";
+		return 0;
+	}
+	int d = b * b - 4 * a * c;
+	if (d > 0)
+	{
+		cout << "The roots are real and distinct.";
+	}
+	else if (d == 0)
+	{
+		cout << "The roots are real and equal.";
+	}
+	else
+	{
+		cout << "The roots are complex.";
+	}
+	return 0;
+}
